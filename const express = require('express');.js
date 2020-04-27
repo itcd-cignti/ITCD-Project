03793a75614d7dae77841f8e54cd66d8a98ff51b
Abhi1006 <53849266+Abhi1006@users.noreply.github.com>
2020-04-28 00:41:21 +0200

    added some changes in server.js file

---
+++ const express = require('express');.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const multer = require('multer');
+const app = express();
+const fs = require('fs');
+const Tesseract = require('tesseract.js');
+
--- server.js
+++ server.js
@@ -1,60 +1,77 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const multer = require('multer');
 const app = express();
-const fs = require('fs');
-var Tesseract = require('tesseract.js');
+const Tesseract = require('tesseract.js');
+var fs = require('fs');
+//var images = new images();
+var PNG = require('png-js');
+var QrCode = require('qrcode-reader');
+const images = require('../ITCD-Cigniti/images');
 
-//middlewares
+//middleware
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended : true }));
+app.use(express.urlencoded ({ extended : true}))
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }))
 app.use(express.json());
 
-const PORT = process.env.PORT | 8080;
+const PORT = process.env.PORT | 5000;
 
 var Storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, __dirname + '/images');
-    },
-    filename: (req, file, callback) => {
-        callback(null, file.originalname);
-    }
+  dest: (req, recfile, callback) => {
+    callback(null, __dirname + '/images');
+  },
+  filename: (req, recfile, callback) => {
+    callback(null, recfile, originalname);
+  }
 });
 
 var upload = multer({
-    storage: Storage
-}).single('image');
-//route
+  storage: Storage
+}).single('images');
+
+var upload = multer({dest: './upload'});
+var type = upload.single('file');
+
+//routes
 app.get('/', (req, res) => {
-    res.render('index');
-});
+  res.render('index');
+})
 
-app.post('/upload', (req, res) => { 
-    console.log(req.file);
-    upload(req, res, err => {
-        if(err) {
-            console.log(err);
-            return tres.send('Something went wrong');
-        }
-
-        var image = fs.readFileSync(
-            __dirname + '/images/' + req.file.originalname,
-            {
-                encoding: null
-            }
-        );
-        Tesseract.recognize(image)
-        .progress(function(p) {
-            console.log('progress',p);
-        })
-        .then(function(result) {
-            res.send(result.html);
-        });
+app.post('/upload', upload.single('file'), function (req, res) {
+  console.log(req.file);
+  res.send('file saved on server');
+    var images = fs.readFileSync(
+      __dirname + '/images.jpeg' + '/images.jpg' +'/images.png' + req.file.originalname,
+      {
+        encoding: null
+      }
+    );
+    Tesseract.recognize(images)
+    .progress(function(p){
+      console.log('progress', p)
+    })
+    .then(function(result) {
+    res.send(result.html);
     });
 });
 
+// app.post('/upload', multer(multerConfig).single('myfile'),function(req, res){
+//   console.log(req.file);
+//   res.sendStatus(200)
+//   Tesseract.recognize(image)
+//   .progress(function(p){
+//     console.log('progress', p)
+//   })
+//   .then(function(result){
+//     res.send(result.html)
+//   })
+// });
+
 app.get('/showdata', (req, res) => {});
 
-app.listen(PORT, () => {
-    console.log(`Server running on Port ${PORT}`);
+app.listen(PORT, () =>{
+  console.log(`Server running on Port ${PORT}`)
 });
